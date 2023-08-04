1eb5412333500895ef6fa532abfbeecbe4ab7446
kwehen <110314567+kwehen@users.noreply.github.com>
2023-08-04 09:36:10 -0400

    Update server.py
    Server checks for a hashed password and allows or disallows access. This can be changed.

--- sockets/server.py
+++ sockets/server.py
@@ -1,4 +1,8 @@
 import socket
+import bcrypt
+
+pw = "Password123"
+hashed = bcrypt.hashpw(pw.encode(), bcrypt.gensalt())
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -6,23 +10,30 @@ host = "127.0.0.1"
 port = 9001
 
 s.bind((host, port))
-
 s.listen()
 
 client, addr = s.accept()
 print(f"Connection received from {addr}")
 while True:
-    data = input('Enter a message:')
+    data = "Welcome to the Server, what is the password?"
     client.sendall(str.encode(data))
     msg = client.recv(1024).decode()
+    if bcrypt.checkpw(msg.encode(), hashed):
+        data = ("Access Granted, Continue...")
+        client.sendall(str.encode(data))
+        while True:
+            msg = client.recv(1024).decode()
+            print(msg)
+            data = input()
+            client.sendall(str.encode(data))
+            if msg == "":
+                client.close()
+                print(f"Connection with {addr} closed.")
+    else:
+        data = ("Access Denied, Goodbye.")
+        client.sendall(str.encode(data))
+        client.close()
     if not msg:
         break
-    print(msg)
-
 client.close()
-print(f'Connection with {addr} has been closed.')
-
-# data = "This is a test, thank you for your connection"
-# client.sendall(str.encode(data))
-# client.close
-# print(f'Connection with {addr} has been closed')
+print(f"Connection with {addr} closed.")
