7684fbd9409acb08f4b5d9edf5119fe1b5129f87
kwehen <110314567+kwehen@users.noreply.github.com>
2023-08-09 12:52:34 -0400

    Update test-ocr.py
    Changed images_from_path so the list is not static

--- test-ocr.py
+++ test-ocr.py
@@ -3,6 +3,7 @@ import numpy as np
 import pytesseract
 from pdf2image import convert_from_path, convert_from_bytes
 
+images_from_path = []
 pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 poppler_path = r"C:/Users/USER/Downloads/Poppler/poppler-23.07.0/Library/bin"
 
@@ -11,8 +12,7 @@ pages = convert_from_bytes(open("C:/Users/USER/Downloads/FILENAME.pdf", "rb").re
 for i, page in enumerate(pages):
     fname = "image"+str(i)+'.png'
     page.save(fname, "PNG")
-
-images_from_path = ["image0.png", "image1.png", "image2.png"]
+    images_from_path.append(fname)
 
 for image in images_from_path:
     img = Image.open(image)
